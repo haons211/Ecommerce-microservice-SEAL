c205f31d845435dae7e926c64b7ee763c72f52b8
haons211 <syhaoc2dh@gmail.com>
2024-10-11 23:07:21 +0700

    first

---
+++ src/test/java/com/haons/microservice/ecommercemicro/EcommerceMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haons.microservice.ecommercemicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
